f10679d240be5d498fd43dc7915f5f42ac3c0418
Valentin Ivanov <vi@Valentins-Mini.attlocal.net>
2023-08-11 10:59:08 -0400

    verstion 1.2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom"
 import Form from "../Form/Form"
 import Image from "../../assets/code.jpg"
 import Rea from "../../assets/React.png"
@@ -7,7 +6,7 @@ import Node from "../../assets/NodeJS.png"
 import Redux from "../../assets/Redux.png"
 import Php from "../../assets/PHP.png"
 import Laravel from "../../assets/Laravel.png"
-import "../Home/home.css"
+import "./home.css"
  
 function Home() {
   return (
@@ -30,56 +29,56 @@ function Home() {
     </article>
     <article className="card-section">
     <div className="grid-container">
-    <div class="card">
-    <div class="card-logo">
+    <div className="card">
+    <div className="card-logo">
       <img src={Rea} alt="Card Logo"/>
     </div>
-    <div class="card-content">
+    <div className="card-content">
       <h3>React</h3>
       <p>React. js, more commonly known as React, is a free, open-source JavaScript library. It works best to build user interfaces by combining sections of code (components) into full websites. Originally built by Facebook, Meta and the open-source community now maintain it.</p>
     </div>
     </div>
-    <div class="card">
-    <div class="card-logo">
+    <div className="card">
+    <div className="card-logo">
       <img src={Js} alt="Card Logo"/>
     </div>
-    <div class="card-content">
+    <div className="card-content">
       <h3>JavaScript</h3>
       <p>JavaScript is a lightweight programming language that web developers commonly use to create more dynamic interactions when developing web pages, applications, servers, and or even games.</p>
     </div>
   </div>
-  <div class="card">
-    <div class="card-logo">
+  <div className="card">
+    <div className="card-logo">
       <img src={Node} alt="Card Logo"/>
     </div>
-    <div class="card-content">
+    <div className="card-content">
       <h3>Node JS</h3>
       <p>Node. js (Node) is an open source, cross-platform runtime environment for executing JavaScript code.</p>
     </div>
   </div>
-  <div class="card">
-    <div class="card-logo">
+  <div className="card">
+    <div className="card-logo">
       <img src={Redux} alt="Card Logo"/>
     </div>
-    <div class="card-content">
+    <div className="card-content">
       <h3>Redux</h3>
       <p>State management tool with React.</p>
     </div>
   </div>
-  <div class="card">
-    <div class="card-logo">
+  <div className="card">
+    <div className="card-logo">
       <img src={Php} alt="Card Logo"/>
     </div>
-    <div class="card-content">
+    <div className="card-content">
       <h3>PHP</h3>
       <p>PHP (recursive acronym for PHP: Hypertext Preprocessor ) is a widely-used open source general-purpose scripting language that is especially suited for web development and can be embedded into HTML.</p>
     </div>
   </div>
-  <div class="card">
-    <div class="card-logo">
+  <div className="card">
+    <div className="card-logo">
       <img src={Laravel} alt="Card Logo"/>
     </div>
-    <div class="card-content">
+    <div className="card-content">
       <h3>Laravel</h3>
       <p>PHP framework, which is robust and easy to understand.</p>
     </div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,17 @@
-import {Link} from "react-router-dom"
-import "../Nav/nav.css"
+import { Link } from "react-router-dom"
+import "./nav.css"
 
 function Nav() {
   return (
     <div className="Container">
       <div className="Logo">
-        <Link to="/">IT HOUSE GROUP</Link>
+        <Link id="nav-links" to="/">IT HOUSE GROUP</Link>
       </div>
       <div className="Left-site_countainer">
         <ul className="List-items">
-          <li className="Home"><Link to="/">HOME</Link></li>
-          <li className="about-item"><Link to="/About">ABOUT US</Link></li>
-          <li className="contacts-item"><Link to="/Contacts">CONTACTS</Link></li>
+          <li className="Home"><Link id="nav-links" to="/">HOME</Link></li>
+          <li className="about-item"><Link id="nav-links" to="/About">ABOUT US</Link></li>
+          <li className="contacts-item"><Link id="nav-links" to="/Contacts">CONTACTS</Link></li>
         </ul>
       </div>
     </div>
